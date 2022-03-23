e199499abda0520befef28fe0e80eb3a0d648204
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-03-23 12:45:21 +0100

    correccion examen

--- Angular/2022/openLibra/src/app/secciones/secciones.module.ts
+++ Angular/2022/openLibra/src/app/secciones/secciones.module.ts
@@ -10,7 +10,7 @@ import { LibrosComponent } from './libros/libros.component';
   declarations: [
     AutoresComponent,
     CategoriasComponent,
-    LibrosComponent
+    LibrosComponent,
   ],
   imports: [
     CommonModule,
