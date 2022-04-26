fa87c7d27c1ba4c8053c5c74b49e416d278a80ee
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-04-26 13:50:50 +0200

    sdlk

--- Angular/2022/openLibra/src/app/estructura/estructura.module.ts
+++ Angular/2022/openLibra/src/app/estructura/estructura.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { EstructuraRoutingModule } from './estructura-routing.module';
+import { MainMenuComponent } from './main/main-menu/main-menu.component';
+import { MainCuerpoComponent } from './main/main-cuerpo/main-cuerpo.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    MainMenuComponent,
+    MainCuerpoComponent
   ],
   imports: [
     CommonModule,
